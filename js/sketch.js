d3864650d05972380ad0ff9f1666b856d2118d84
Robbo270 <116882868+Robbo270@users.noreply.github.com>
2022-11-25 12:15:52 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,26 @@
+var x, y, d, speed;
+
 function setup() {
     var canvas = createCanvas(800, 800);
     canvas.parent("p5container");
+    x = width/2;
+    y = height/2;
+    d = 200;
+    speed = 5;
 }
 
 function draw() {
     background(220);
 
+    //y++;
+
+    speed = map(mouseY, 0, height, 5, 50)
+    
+    y = y + speed
+    
+    if(y > height + d/2){
+        y = 0 - d/2;
+    }
     fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    ellipse(x, y, d);
 }
